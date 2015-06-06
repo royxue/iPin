864d69d0eda2686fe9b011ab3644cde08950ed9f
Lijun Xue <xljroy@gmail.com>
2015-06-06 20:26:10 +0800

    Update User Part

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -238,7 +238,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :github, 'b1d53a2037832639b70b', '174a88043dd1ed184260dbc00ea6a034179b30e8', scope: 'user,public_repo'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
